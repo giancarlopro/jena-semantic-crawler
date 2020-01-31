3895111ad9ec5ff8afe1cb246909231e86a34e14
Giancarlo Rocha <giancarloiff@gmail.com>
2020-01-31 11:17:19 -0300

    add: jena hello world

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,20 @@
 package app;
 
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+
 public class App {
+    static private String namespace = "http://example.org/test/";
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        Model model = ModelFactory.createDefaultModel();
+
+        Resource subject = model.createResource(namespace + "message");
+        Property property = model.createProperty(namespace + "says");
+        subject.addProperty(property, "Hello World!", XSDDatatype.XSDstring);
+
+        model.write(System.out);
     }
 }
